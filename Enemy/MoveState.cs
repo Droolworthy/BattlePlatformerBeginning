fdc6ac54bf01d4c5458924fb687bdf8bf7942313
Droolworthy <107348019+Droolworthy@users.noreply.github.com>
2024-01-11 14:02:00 +0300

    Update MoveState.cs

--- Enemy/MoveState.cs
+++ Enemy/MoveState.cs
@@ -1,27 +1,12 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
-public class Bullet : MonoBehaviour
+public class MoveState : State
 {
     [SerializeField] private float _speed;
 
-    private Transform _target;
-    private Rigidbody _rigidbody;
-
-    private void Start()
-    {
-        _rigidbody = GetComponent<Rigidbody>();    
-    }
-
     private void Update()
     {
-        var direction = (_target.position - transform.position).normalized;
-
-        _rigidbody.velocity = direction * _speed;
-    }
-
-    public void Init(Transform target)
-    {
-        _target = target;
+        transform.position = Vector2.MoveTowards(transform.position, Target.transform.position,
+            _speed * Time.deltaTime);
     }
 }
